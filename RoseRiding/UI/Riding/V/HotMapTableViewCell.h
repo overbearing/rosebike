a558f12c2e21ab523d554109928a8a0c06a78b66
luanweihao <luanweihao@foxmail.com>
2020-11-23 11:55:16 +0800

    2020年11月23日11:55:12

---
+++ RoseRiding/UI/Riding/V/HotMapTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  HotMapTableViewCell.h
+//  RoseRiding
+//
+//  Created by 蜡笔小新 on 2020/10/9.
+//  Copyright © 2020 MR_THT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HotMapTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RoseRiding/UI/Riding/V/HotMapTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HotMapTableViewCell.m
+//  RoseRiding
+//
+//  Created by 蜡笔小新 on 2020/10/9.
+//  Copyright © 2020 MR_THT. All rights reserved.
+//
+
+#import "HotMapTableViewCell.h"
+
+@implementation HotMapTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
